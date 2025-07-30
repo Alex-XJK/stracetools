2458dadc96a22219c436783624b18fe53b704501
Alex Jiakai Xu <ax2155@columbia.edu>
2025-07-29 23:37:43 -0400

    Draft project layout

---
+++ stracetools/__init__.py
@@ -0,0 +1,12 @@
+"""
+Strace Tools - A Python library for parsing and analyzing strace output
+"""
+
+__version__ = "0.1.0"
+__author__ = "Alex Jiakai Xu"
+__email__ = "jiakai.xu@columbia.edu"
+
+# Import main classes for easy access
+
+
+__all__ = []
